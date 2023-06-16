aff6bfd2718d270c4e5560702144ccc1992e64c9
Bilal Hassan <bilal.unleashed@gmail.com>
2023-06-16 01:00:56 +0100

    First Commit

---
+++ app/src/main/java/bilal/altify/presentation/MainActivity.kt
@@ -0,0 +1,32 @@
+package bilal.altify.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import bilal.altify.presentation.theme.AltifyTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AltifyTheme {
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AltifyTheme {
+    }
+}
